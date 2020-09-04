2468768d1390ea67821793064e7b1e0bc64ad06d
Yura Linkov <wall-webdev@yandex.ru>
2020-09-04 12:51:08 +0300

    fix после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,48 @@
-let edit = document.querySelector('.about__profile-edit');
-let popupOpened = document.querySelector('.popup');
-let popupClose = document.querySelector('.popup__close');
-let formBtnSave = document.querySelector('.form__btn');
-
-edit.addEventListener('click', function () {
-  popupOpened.classList.add('popup_opened');
-});
-
-popupClose.addEventListener('click', function () {
-  popupOpened.classList.remove('popup_opened');
-});
+let buttonEditProfile = document.querySelector('.about__profile-edit');
+let popupWindow = document.querySelector('.popup');
+let buttonClosePopup = document.querySelector('.popup__close');
+let buttonSubmitPopup = document.querySelector('.form__btn');
 
 // Находим форму в DOM
-let formElement = document.querySelector('.form'); // Воспользуйтесь методом querySelector()
+let formElement = document.querySelector('.form');
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.form__item_type_name');
+let professionInput = document.querySelector('.form__item_type_profession');
+// Выбераем элементы, куда должны быть вставлены значения полей
+let aboutName = document.querySelector('.about__title');
+let aboutProfession = document.querySelector('.about__subtitle');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+function popupOpen() {
+  popupWindow.classList.add('popup_opened');
+}
+
+function popupClose() {
+  popupWindow.classList.remove('popup_opened');
+}
+
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
 
-  // Находим поля формы в DOM
-  let nameInput = document.querySelector('.form__item_type_name'); // Воспользуйтесь инструментом .querySelector()
-  let jobInput = formElement.querySelector('.form__item_type_profession'); // Воспользуйтесь инструментом .querySelector()
-
-  // Получите значение полей из свойства value
-  let name = nameInput.value;
-  let job = jobInput.value;
   // Выберите элементы, куда должны быть вставлены значения полей
-  let aboutName = document.querySelector('.about__title');
-  let aboutJob = document.querySelector('.about__subtitle');
   // Вставьте новые значения с помощью textContent
-  aboutName.textContent = name;
-  aboutJob.textContent = job;
+  aboutName.textContent = nameInput.value;
+  aboutProfession.textContent = professionInput.value;
+  popupClose();
 }
+
+function listenEditButton() {
+  nameInput.value = aboutName.textContent;
+  professionInput.value = aboutProfession.textContent;
+}
+
+buttonEditProfile.addEventListener('click', popupOpen);
+buttonClosePopup.addEventListener('click', popupClose);
+
 // Прикрепляем обработчик к форме:
-formBtnSave.addEventListener('click', function () {
-  popupOpened.classList.remove('popup_opened');
-});
+buttonSubmitPopup.addEventListener('click', formSubmitHandler);
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+buttonEditProfile.addEventListener('click', listenEditButton);
